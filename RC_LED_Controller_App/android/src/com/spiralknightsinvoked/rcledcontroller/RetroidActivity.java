ec1b29ef82626cf0fb83a24838b7b6f60140f48c
TorusXO <Torusiii@bk.ru>
2026-08-04 02:46:48 +0700

    Map controller menu buttons to settings

--- RC_LED_Controller_App/android/src/com/spiralknightsinvoked/rcledcontroller/RetroidActivity.java
+++ RC_LED_Controller_App/android/src/com/spiralknightsinvoked/rcledcontroller/RetroidActivity.java
@@ -89,6 +89,12 @@ public class RetroidActivity extends QtActivity {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             return KeyEvent.KEYCODE_ESCAPE;
         }
+        if (keyCode == KeyEvent.KEYCODE_BUTTON_MODE ||
+            keyCode == KeyEvent.KEYCODE_BUTTON_SELECT ||
+            keyCode == KeyEvent.KEYCODE_BUTTON_START ||
+            keyCode == KeyEvent.KEYCODE_MENU) {
+            return KeyEvent.KEYCODE_F1;
+        }
         if (keyCode == KeyEvent.KEYCODE_BUTTON_A) {
             return KeyEvent.KEYCODE_ENTER;
         }
